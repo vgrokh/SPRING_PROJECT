d59f72c8f56c5464a6d5d05c855f60b66032f419
victor90poe <victor.grokh@90poe.io>
2021-02-26 21:14:43 +0200

    Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+java $JAVA_OPTS -Dserver.port=$PORT -jar target/springcrud-0.0.1-SNAPSHOT.jar
--- src/main/java/com/vgrokh/springcrud/SpringcrudApplication.java
+++ src/main/java/com/vgrokh/springcrud/SpringcrudApplication.java
@@ -2,13 +2,11 @@ package com.vgrokh.springcrud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
 public class SpringcrudApplication {
 
     public static void main(String[] args) {
-        System.getProperties().put( "server.port", System.getenv().get("PORT"));
         SpringApplication.run(SpringcrudApplication.class, args);
     }
 
